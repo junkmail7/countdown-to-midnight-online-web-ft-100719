9c0a93bd3536d6c92e3b64d187f90705bfd3d135
William Pannell <lpann5@gmail.com>
2019-10-13 20:25:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,14 @@ def countdown(x=0)
   end
   puts "HAPPY NEW YEAR!\n"
 end
+
+def countdown_with_sleep(x=0)
+  x = 10
+  while x !=0
+  puts "#{x} SECOND(S)!\n"
+  sleep(1)
+  x -= 1
+  end
+  puts "HAPPY NEW YEAR!\n"
+end
+  
